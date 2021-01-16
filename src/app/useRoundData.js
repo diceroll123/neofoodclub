00ed0089d7529ab0045f844d0feaa135d2bf1da4
Steve C <diceroll123@gmail.com>
2021-01-16 02:12:28 -0800

    Implement a more granular firebase update method

--- src/app/useRoundData.js
+++ src/app/useRoundData.js
@@ -10,7 +10,7 @@ export default function useRoundData(firebase, currentSelectedRound) {
     // When the page first loads, if there's no round selected yet, get the current round number
     // from Firebase.
     useEffect(() => {
-        firebase.database().ref().child("current_round").once('value', (snapshot) => {
+        firebase.database().ref().child("current_round").once('value').then((snapshot) => {
             const newCurrentRound = snapshot.val();
             setCurrentRound(newCurrentRound);
         });
@@ -24,17 +24,47 @@ export default function useRoundData(firebase, currentSelectedRound) {
 
         const ref = firebase.database().ref().child(`rounds/${currentSelectedRound}`);
 
-        const onValueChange = (snapshot) => {
+        const update = (snapshot, name) => {
+            const value = snapshot.val();
+            if (value === null) {
+                // invalid round is the only way this will happen
+                return;
+            }
+            setRoundData(roundData => {
+                const newObj = {};
+                newObj[name] = snapshot.val();
+                return {
+                    ...roundData, ...newObj
+                }
+            })
+        }
+
+        const onWinnersChange = (snapshot) => {
+            update(snapshot, "winners");
+
+            // And, if this this *seems* like the current round but it has winning pirates, then
+            // that means it just ended, and there should be a new current round now. Increment
+            // the counter, so the user can navigate to it if they want!
+            if (currentRound === currentSelectedRound && snapshot.val()[0] > 0) {
+                setCurrentRound(currentRound => currentRound + 1);
+            }
+        };
+        const winnersRef = ref.child("winners");
+
+        const onCurrentOddsChange = (snapshot) => update(snapshot, "currentOdds");
+        const currentOddsRef = ref.child("currentOdds");
+
+        const onTimestampChange = (snapshot) => update(snapshot, "timestamp");
+        const timestampRef = ref.child("timestamp");
+
+        const onLastChangeChange = (snapshot) => update(snapshot, "lastChange");
+        const lastChangeRef = ref.child("lastChange");
+
+        // get the entire round state once
+        ref.once('value').then((snapshot) => {
             let newRoundData = snapshot.val();
             if (newRoundData) {
                 setRoundData(newRoundData);
-
-                // And, if this this *seems* like the current round but it has winning pirates, then
-                // that means it just ended, and there should be a new current round now. Increment
-                // the counter, so the user can navigate to it if they want!
-                if (currentRound === currentSelectedRound && newRoundData.winners[0] > 0) {
-                    setCurrentRound(currentRound => currentRound + 1);
-                }
             } else {
                 toast.closeAll();
                 toast({
@@ -45,10 +75,21 @@ export default function useRoundData(firebase, currentSelectedRound) {
                     isClosable: true
                 });
             }
+        }).then(() => {
+            // and now we grab the changeable data points rather than the entire state every time
+            winnersRef.on('value', onWinnersChange);
+            currentOddsRef.on('value', onCurrentOddsChange);
+            timestampRef.on('value', onTimestampChange);
+            lastChangeRef.on('value', onLastChangeChange);
+        });
+
+        return () => {
+            winnersRef.off('value', onWinnersChange);
+            currentOddsRef.off('value', onCurrentOddsChange);
+            timestampRef.off('value', onTimestampChange);
+            lastChangeRef.off('value', onLastChangeChange);
         };
 
-        ref.on('value', onValueChange);
-        return () => ref.off("value", onValueChange);
     }, [
         toast,
         firebase,
