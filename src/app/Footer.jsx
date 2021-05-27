09a80067426e500fa6746836f3f85a35806c7f31
Steve C <diceroll123@gmail.com>
2021-05-27 19:39:12 -0400

    Add react-icons + tweak site footer

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -8,15 +8,7 @@ import {
     Link,
     Tooltip,
 } from "@chakra-ui/react";
-
-const GithubIcon = (props) => (
-    <svg viewBox="0 0 20 20" {...props}>
-        <path
-            fill="currentColor"
-            d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"
-        />
-    </svg>
-);
+import { FaGithub, FaCoffee } from "react-icons/fa";
 
 function FooterLink(props) {
     const { icon, href, label } = props;
@@ -31,7 +23,12 @@ function FooterLink(props) {
 
 const links = [
     {
-        icon: GithubIcon,
+        icon: FaCoffee,
+        label: "Donate/Buy me a coffee",
+        href: "https://paypal.me/diceroll123",
+    },
+    {
+        icon: FaGithub,
         label: "GitHub",
         href: "https://github.com/diceroll123/neofoodclub",
     },
@@ -44,18 +41,24 @@ function Footer(props) {
             <Box pt={6}>
                 <Text fontSize="sm">
                     Made by{" "}
-                    <Link
-                        href="https://www.reddit.com/user/diceroll123"
-                        isExternal
-                    >
-                        u/diceroll123
+                    <Link href="https://github.com/diceroll123" isExternal>
+                        diceroll123
                     </Link>
                 </Text>
-                <HStack mt={4} spacing="12px" justify="center">
+                <HStack m={4} spacing="12px" justify="center">
                     {links.map((link) => (
                         <FooterLink key={link.href} {...link} />
                     ))}
                 </HStack>
+                <Text fontSize="sm">
+                    Website, design, and code © neofood.club
+                </Text>
+                <Text fontSize="sm">
+                    This is an unofficial Neopets fansite with no affiliation/endorsement with Neopets.
+                </Text>
+                <Text fontSize="sm">
+                    © Neopets, Inc. All rights reserved. Used With Permission
+                </Text>
             </Box>
         </Box>
     );
