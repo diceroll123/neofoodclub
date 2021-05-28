9b86e34cae36e397b3079e7695251d618d5c661a
Steve C <diceroll123@gmail.com>
2021-05-28 02:16:20 -0400

    Add PWA capabilities
    Not sure if this'll work, has to be deployed to find out! 🤞

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/App";
 import FaviconGenerator from "./app/FaviconGenerator";
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
-        <FaviconGenerator/>
-        <App/>
+        <FaviconGenerator />
+        <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
