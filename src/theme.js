d7c6e1021c08492edc2867232b6c170021b5bee1
Steve C <diceroll123@gmail.com>
2022-02-12 21:09:09 -0500

    Add default color mode (dark) but use system setting first (#259)

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: true,
+    colors: {
+        nfc: {
+            blue: "#90CDF4",
+            blueDark: "#4BA0E4",
+
+            gray: "#F7FAFC",
+            grayDark: "#2D3748",
+
+            green: "#9AE6B4",
+            greenDark: "#50C17F",
+
+            red: "#FEB2B2",
+            redDark: "#F76C6C",
+
+            orange: "#FBD38D",
+            orangeDark: "#F0923E",
+
+            yellow: "#FAF089",
+            yellowDark: "#EFCF50",
+        },
+    },
+}
+
+const theme = extendTheme({ config });
+
+export default theme;
