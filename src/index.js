1c7c2ec5bef06b739aac5e665b3e6698bd8ba8c1
Steve C <diceroll123@gmail.com>
2022-02-12 21:06:32 -0500

    Add default color mode (dark) but use system setting first

--- src/index.js
+++ src/index.js
@@ -6,31 +6,8 @@ import FaviconGenerator from "./app/FaviconGenerator";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { StateProvider } from "./app/RoundState";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-    colors: {
-        nfc: {
-            blue: "#90CDF4",
-            blueDark: "#4BA0E4",
-
-            gray: "#F7FAFC",
-            grayDark: "#2D3748",
-
-            green: "#9AE6B4",
-            greenDark: "#50C17F",
-
-            red: "#FEB2B2",
-            redDark: "#F76C6C",
-
-            orange: "#FBD38D",
-            orangeDark: "#F0923E",
-
-            yellow: "#FAF089",
-            yellowDark: "#EFCF50",
-        },
-    },
-});
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
 ReactDOM.render(
     <React.StrictMode>
