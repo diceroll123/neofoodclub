bbe67b2c4b054fc18354d4ed486e333828d75e62
Steve C <diceroll123@gmail.com>
2024-05-21 02:18:31 -0400

    Update footer

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,33 +1,20 @@
 import {
-    ButtonGroup,
-    IconButton,
     Box,
+    Center,
     Stack,
     Text,
-    Tooltip,
     Heading,
-    Icon,
     Divider,
     Link,
+    Container,
     useColorModeValue,
+    SimpleGrid,
+    Flex,
 } from "@chakra-ui/react";
 import * as React from "react";
 import NeopointIcon from "./images/np-icon.svg";
-import { FaGithub, FaCoffee } from "react-icons/fa";
 import { VercelCredit } from "./components/VercelCredit";
 
-const links = [
-    {
-        icon: FaCoffee,
-        label: "Donate/Buy me a coffee",
-        href: "https://ko-fi.com/dice",
-    },
-    {
-        icon: FaGithub,
-        label: "GitHub",
-        href: "https://github.com/diceroll123/neofoodclub",
-    },
-];
 
 function Logo() {
     return (
@@ -45,71 +32,88 @@ function Logo() {
     );
 }
 
-const Copyright = (props) => (
-    <Text fontSize="sm" {...props}>
-        Website, design, and code &copy; neofood.club
-        <br />
-        This is an unofficial Neopets fansite with no affiliation/endorsement
-        with Neopets.
-        <br /> Images/Names &copy; Neopets, Inc. All rights reserved. Used With
-        Permission
-    </Text>
-);
-
-function FooterButton(props) {
-    const { icon, href, label } = props;
+function ListHeader(props) {
     return (
-        <Tooltip label={label} aria-label={label}>
-            <IconButton
-                as={Link}
-                href={href}
-                isExternal
-                fontSize="xl"
-                icon={<Icon as={icon} />}
-            />
-        </Tooltip>
+        <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+            {props.children}
+        </Text>
     );
+};
+
+function KoFiButtion() {
+    return (
+        <>
+            <Link href='https://ko-fi.com/dice' isExternal>
+                <img
+                    height='36'
+                    style={{ border: '0px', height: '36px' }}
+                    src='https://storage.ko-fi.com/cdn/kofi1.png?v=3'
+                    border='0'
+                    alt='Buy Me a Coffee at ko-fi.com' />
+            </Link>
+        </>
+    )
 }
 
-function FooterButtons(props) {
-    return (
-        <ButtonGroup variant="ghost" color="gray.400" {...props}>
-            {links.map((link) => (
-                <FooterButton key={link.href} {...link} />
-            ))}
-        </ButtonGroup>
-    );
-}
-
-function Footer(props) {
+export default function Footer() {
     return (
         <>
             <Divider pt={6} />
             <Box
                 bg={useColorModeValue('gray.50', 'gray.900')}
-                as="footer"
-                role="contentinfo"
-                mx="auto"
-                maxW="full"
-                py="12"
-                px={{ base: "4", md: "8" }}
-            >
-                <Stack>
-                    <Stack
-                        direction="row"
-                        spacing="4"
-                        align="center"
-                        justify="space-between"
-                    >
+                color={useColorModeValue('gray.700', 'gray.200')}>
+                <Container as={Stack} maxW={'6xl'} py={10}>
+                    <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={8}>
+                        <Stack align={'flex-start'}>
+                            <ListHeader>Food Club Links</ListHeader>
+                            <Link isExternal href='https://www.neopets.com/pirates/foodclub.phtml?type=bet'>Place Bets</Link>
+                            <Link isExternal href='https://www.neopets.com/pirates/foodclub.phtml?type=current_bets'>Current Bets</Link>
+                            <Link isExternal href='https://www.neopets.com/pirates/foodclub.phtml?type=collect'>Collect Winnings</Link>
+                            <Link isExternal href='https://www.neopets.com/gamescores.phtml?game_id=88'>High Score List</Link>
+                        </Stack>
+
+                        <Stack align={'flex-start'}>
+                            <ListHeader>NeoFoodClub Stuff</ListHeader>
+                            <Link isExternal href='https://foodclub.neocities.org/'>Classic NeoFoodClub</Link>
+                            <Link isExternal href='https://github.com/diceroll123/neofoodclub'>Source Code</Link>
+                        </Stack>
+
+                        <Stack align={'flex-start'}>
+                            <ListHeader>Support us</ListHeader>
+                            <KoFiButtion />
+                        </Stack>
+                    </SimpleGrid>
+                </Container>
+                <Box py={10}>
+                    <Flex
+                        align={'center'}
+                        _before={{
+                            content: '""',
+                            borderBottom: '1px solid',
+                            borderColor: useColorModeValue('gray.200', 'gray.700'),
+                            flexGrow: 1,
+                            mr: 8,
+                        }}
+                        _after={{
+                            content: '""',
+                            borderBottom: '1px solid',
+                            borderColor: useColorModeValue('gray.200', 'gray.700'),
+                            flexGrow: 1,
+                            ml: 8,
+                        }}>
                         <Logo />
-                        <FooterButtons />
-                    </Stack>
-                    {<Copyright alignSelf={{ base: "center", sm: "start" }} />}
-                </Stack>
-                <VercelCredit />
+                    </Flex>
+                    <Text pt={6} fontSize={'sm'} textAlign={'center'}>
+                        Website, design, and code &copy; neofood.club
+                        <br />
+                        This is an unofficial Neopets fansite with no affiliation/endorsement
+                        with Neopets.
+                        <br /> Images/Names &copy; Neopets, Inc. All rights reserved. Used With
+                        Permission
+                    </Text>
+                    <Center><VercelCredit /></Center>
+                </Box>
             </Box>
         </>
     );
 }
-
-export default Footer;
