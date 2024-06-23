fb27cead3fefaf87c7109eb59a699efcdb20b93c
Steve C <diceroll123@gmail.com>
2024-06-23 19:09:23 -0400

    tweak extrabox theme usage

--- src/app/components/ExtraBox.jsx
+++ src/app/components/ExtraBox.jsx
@@ -1,15 +1,11 @@
-import { useTheme, useColorModeValue, Box } from "@chakra-ui/react";
+import { useColorModeValue, Box } from "@chakra-ui/react";
 import React from "react";
 
 // this element is the darker box inside settingsbox, for example the container of the normal/dropdown mode radio buttons
 
 const ExtraBox = (props) => {
   const { children, ...rest } = props;
-  const theme = useTheme();
-  const defaultBgColor = useColorModeValue(
-    theme.colors.white,
-    theme.colors.gray["800"]
-  );
+  const defaultBgColor = useColorModeValue("white", "gray.800");
   return (
     <Box
       p={2}
