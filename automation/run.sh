69306d4f33fc9ff92e0624c7b44cbd9b3757f6fc
Steve C <diceroll123@gmail.com>
2024-06-24 00:42:51 -0400

    move automation output

--- automation/run.sh
+++ automation/run.sh
@@ -4,12 +4,12 @@ docker build -t nfc_values .
 # Run the docker container
 docker run --rm -v $(pwd):/app nfc_values
 
-# copy file from ./output/javascript.js to ../src/app/constants_logit.js
-cp ./output/javascript.js ../src/app/constants_logit.js
+# copy file from ./output/javascript.js to ../app/constants_logit.js
+cp ./output/javascript.js ../app/constants_logit.js
 
 # stage the changes
 git add ./output/
-git add ../src/app/constants_logit.js
+git add ../app/constants_logit.js
 
 # commit the changes
 git commit -m "Auto-update logit constants"
