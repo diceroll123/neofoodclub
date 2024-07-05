62870a47f5e700100080d26a9ac4ad04206f8314
Steve C <diceroll123@gmail.com>
2024-07-04 23:01:34 -0400

    make fetch api

---
+++ api/fetch-round.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const { BUCKET_NAME } = process.env;
+
+module.exports = async (req, res) => {
+  const { round } = req.query;
+  if (!round) {
+    return res.status(400).json({ error: "Round parameter is required" });
+  }
+
+  try {
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: `rounds/${round}.json`,
+    };
+    const data = await s3.getObject(params).promise();
+    const jsonData = JSON.parse(data.Body.toString("utf-8"));
+
+    res.status(200).json(jsonData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+};
