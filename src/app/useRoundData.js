14c63bf7def88eee8516656582d43ffd61fde60f
Steve C <diceroll123@gmail.com>
2024-07-06 00:35:33 -0400

    remove firebase

--- src/app/useRoundData.js
+++ src/app/useRoundData.js
@@ -1,132 +1,80 @@
 import { useState, useEffect } from "react";
 import { useToast } from "@chakra-ui/react";
-import {
-  getDatabase,
-  ref,
-  get,
-  child,
-  goOffline,
-  goOnline,
-  off,
-  onValue,
-} from "firebase/database";
 
-export default function useRoundData(firebase, currentSelectedRound) {
+export default function useRoundData(currentSelectedRound) {
   const toast = useToast();
 
   const [currentRound, setCurrentRound] = useState(null);
   const [roundData, setRoundData] = useState(null);
 
   // When the page first loads, if there's no round selected yet, get the current round number
-  // from Firebase.
   useEffect(() => {
-    get(child(ref(getDatabase(firebase)), "current_round")).then((snapshot) => {
-      const newCurrentRound = snapshot.val();
-      setCurrentRound(parseInt(newCurrentRound));
-    });
-  }, [firebase]);
+    const fetchCurrentRound = async () => {
+      const currentRoundResponse = await fetch("/api/fetch-current-round");
+      const currentRoundData = await currentRoundResponse.text();
+      if (/\d+/.test(currentRoundData)) {
+        setCurrentRound(parseInt(currentRoundData));
+      }
+    };
+    fetchCurrentRound();
+  }, []);
 
-  // Once a round is selected, we should subscribe to its data!
+  // Once a round is selected, we should poll its data!
   useEffect(() => {
-    if (!currentSelectedRound || !currentRound) {
+    if (!currentSelectedRound) {
       return;
     }
 
-    const dbRef = ref(getDatabase(firebase), `rounds/${currentSelectedRound}`);
+    let dontLoop = false;
 
-    const update = (snapshot, name) => {
-      const value = snapshot.val();
-      if (value === null) {
-        // invalid round is the only way this will happen
+    const fetchRoundData = async () => {
+      if (dontLoop) {
         return;
       }
-      setRoundData((roundData) => {
-        // skip the re-render if it's not needed
-        if (
-          roundData &&
-          JSON.stringify(roundData[name]) === JSON.stringify(value)
-        ) {
-          return roundData;
+      try {
+        const response = await fetch(
+          `/api/fetch-round/?round=${currentSelectedRound}`
+        );
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.error);
         }
+        setRoundData(data);
 
-        return {
-          ...roundData,
-          [name]: value,
-        };
-      });
-    };
-
-    const onWinnersChange = (snapshot) => {
-      update(snapshot, "winners");
-
-      // And, if this this *seems* like the current round but it has winning pirates, then
-      // that means it just ended, and there should be a new current round now. Increment
-      // the counter, so the user can navigate to it if they want!
-      if (currentRound === currentSelectedRound && snapshot.val()[0] > 0) {
-        setCurrentRound((currentRound) => currentRound + 1);
+        if (data?.winners[0] > 0) {
+          // we don't need to keep polling if the round has ended
+          dontLoop = true;
+        }
+      } catch (error) {
+        toast({
+          title: `Failed to fetch round ${currentSelectedRound}`,
+          description: "Please try again later.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
       }
     };
-    const winnersRef = child(dbRef, "winners");
-    const currentOddsRef = child(dbRef, "currentOdds");
-    const timestampRef = child(dbRef, "timestamp");
-    const lastChangeRef = child(dbRef, "lastChange");
 
-    const isCurrentOrPreviousRound = () => {
-      return [currentRound, currentRound - 1].includes(
-        parseInt(currentSelectedRound)
-      );
+    fetchRoundData(); // Fetch immediately when the round is selected
+    let interval = setInterval(fetchRoundData, 5000);
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        clearInterval(interval);
+      } else {
+        fetchRoundData(); // Fetch immediately when the page becomes visible
+        interval = setInterval(fetchRoundData, 5000);
+      }
     };
 
-    const addFirebaseHandlers = () => {
-      // grab the changeable data points rather than the entire state every time
-      goOnline(getDatabase(firebase));
-    };
-
-    const removeFirebaseHandlers = () => {
-      goOffline(getDatabase(firebase));
-    };
-
-    // get the entire round state once
-    get(dbRef)
-      .then((snapshot) => {
-        let newRoundData = snapshot.val();
-        if (newRoundData) {
-          setRoundData(newRoundData);
-        } else {
-          toast.closeAll();
-          toast({
-            title: `Round ${currentSelectedRound} not found.`,
-            description: "We don't seem to have data for this round. 🤔",
-            status: "error",
-            duration: 3000,
-            isClosable: true,
-          });
-        }
-      })
-      .then(() => {
-        // after getting round state, add listeners if this is current/previous round
-        if (isCurrentOrPreviousRound()) {
-          onValue(winnersRef, onWinnersChange);
-          onValue(currentOddsRef, (snapshot) =>
-            update(snapshot, "currentOdds")
-          );
-          onValue(timestampRef, (snapshot) => update(snapshot, "timestamp"));
-          onValue(lastChangeRef, (snapshot) => update(snapshot, "lastChange"));
-        }
-      });
-
-    window.addEventListener("blur", removeFirebaseHandlers);
-    window.addEventListener("focus", addFirebaseHandlers);
+    document.addEventListener("visibilitychange", handleVisibilityChange);
 
     return () => {
-      window.removeEventListener("blur", removeFirebaseHandlers);
-      window.removeEventListener("focus", addFirebaseHandlers);
-      off(winnersRef);
-      off(currentOddsRef);
-      off(timestampRef);
-      off(lastChangeRef);
+      clearInterval(interval);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
-  }, [toast, firebase, currentSelectedRound, currentRound]);
+  }, [toast, currentSelectedRound, setRoundData]);
 
   return [currentRound, roundData];
 }
