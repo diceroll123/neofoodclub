acfb56c27622a067f13b2cdc54fc46a8e4483cb9
Steve C <diceroll123@gmail.com>
2024-09-30 02:44:46 -0400

    add wrap to bet amount buttons

--- src/app/components/BetAmountsButtons.jsx
+++ src/app/components/BetAmountsButtons.jsx
@@ -5,6 +5,7 @@ import {
   Heading,
   Stack,
   Tooltip,
+  Wrap,
 } from "@chakra-ui/react";
 import { useContext } from "react";
 import { RoundContext } from "../RoundState";
@@ -67,7 +68,7 @@ const BetAmountsButtons = (props) => {
         <Heading size="sm" textTransform="uppercase">
           Set bet amounts
         </Heading>
-        <ButtonGroup mt={2}>
+        <ButtonGroup as={Wrap} mt={2}>
           <Tooltip
             label="Sets all bet amounts to whichever is lower: your max bet amount, or the value in the MAXBET column below + 1. This prevents you from betting more than necessary to earn 1M NP from the bet, given the current odds."
             openDelay="600"
