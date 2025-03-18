b13085c595268acc5d449156f5e30d9b1e481787
Steve C <diceroll123@gmail.com>
2025-03-18 06:24:57 -0400

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,33 +1,9 @@
-const CracoSwcPlugin = require("craco-swc");
+const MillionLint = require("@million/lint");
 
 module.exports = {
     plugins: [
-        {
-            plugin: CracoSwcPlugin,
-            options: {
-                swcLoaderOptions: {
-                    jsc: {
-                        parser: {
-                            syntax: "ecmascript",
-                            jsx: true,
-                            dynamicImport: true,
-                            exportDefaultFrom: true,
-                        },
-                        transform: {
-                            react: {
-                                runtime: "classic",
-                                development: false,
-                                throwIfNamespace: true,
-                                importSource: "react",
-                                pragma: "React.createElement",
-                                pragmaFrag: "React.Fragment",
-                                useBuiltIns: false,
-                                useSpread: false,
-                            },
-                        },
-                    },
-                },
-            },
-        },
+        MillionLint.craco({
+            legacyHmr: true,
+        }),
     ],
 };
