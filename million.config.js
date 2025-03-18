d5dbe52699d92c2a7cebe664af0e9746d4e5c47f
Steve C <diceroll123@gmail.com>
2025-03-18 06:25:00 -0400

    Create million.config.js

---
+++ million.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Basic Million.js configuration
+  auto: true,
+  // You can customize which files to include/exclude
+  include: ['./src/**/*.{js,jsx,ts,tsx}'],
+  exclude: ['**/node_modules/**', '**/dist/**'],
+  // Set this to false if the extension is still hanging
+  server: {
+    enable: true,
+    port: 3001
+  }
+};
