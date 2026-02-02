ceff827a103f1026d623c244c270c0fbb1836f01
Steve C <diceroll123@gmail.com>
2026-02-02 02:02:14 -0500

    Update HomePage.tsx

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -4,10 +4,12 @@ import React from 'react';
 import EditBets from './components/views/EditBets';
 import Footer from './Footer';
 import Header from './Header';
+import TopProgressBar from './components/ui/TopProgressBar';
 
 const HomePage = React.memo(
   (): React.ReactElement => (
     <>
+      <TopProgressBar />
       <Header />
 
       <Box pt="7rem" w="100%" maxW="100%">
